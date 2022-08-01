4c9ee318c6dcc9e3c855f6cef87db811dcc00fe8
Ali Shirani <alishiranii1384@gmail.com>
2022-08-01 23:12:49 +0430

    added skeleton

--- pages/recipes/[slug].js
+++ pages/recipes/[slug].js
@@ -1,6 +1,7 @@
 import { createClient } from "contentful";
 import Image from "next/image";
 import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
+import Skeleton from "../../components/Skeleton";
 
 const client = createClient({
   space: process.env.SPACE_ID,
@@ -8,6 +9,9 @@ const client = createClient({
 });
 
 export default function RecipeDetail({ recipe }) {
+
+  if(!recipe)return <Skeleton/>
+
   const { featuredImage, title, cookingTime, ingredients, method } =
     recipe.fields;
   return (
@@ -73,7 +77,7 @@ export async function getStaticPaths() {
   });
   return {
     paths,
-    fallback: false,
+    fallback: true,
   };
 }
 export async function getStaticProps({ params }) {
